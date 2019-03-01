ed5d8c9899c45440a4b0891690edb6d7bd789a2a
Andrew Boring <andrew@andrewboring.com>
2019-03-01 15:56:25 -0500

    initial commit

---
+++ notebooks/config.py
@@ -0,0 +1,7 @@
+# Use this file to store your credentials for the code samples if you want.
+# All code samples assume the config.py file is located in the same directory as the code.
+# Replace the CAPITAL LETTERS and keep the quotes.
+
+controller = "YOUR CONTROLLER HOSTNAME/IP!"
+username = "YOUR USERNAME"
+password = "YOUR PASSWORD"
