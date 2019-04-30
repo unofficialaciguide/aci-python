dcad6fc0c3c280638e6e7ee9697ed9498ffa3d63
Andrew Boring <andrew@andrewboring.com>
2019-04-29 21:49:16 -0400

    removed old sample config.py files

--- py/config.py
+++
@@ -1,6 +0,0 @@
-# Use this file to store your credentials for the code samples if you want.
-# All code samples assume the config.py file is located in the same directory as the code.
-
-controller = "wabac-machine.mr-peabody.com"
-username = "sherman"
-password = "and-away-we-go!!!"
